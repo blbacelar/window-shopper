301de44d84cba9c1f564ab17df1d7bea26e3f241
Bruno Bacelar <blbacelar@gmail.com>
2018-01-25 16:20:57 -0800

    Unit Test

---
+++ windows_shopper/Model/Wage.swift
@@ -0,0 +1,17 @@
+//
+//  Wage.swift
+//  windows_shopper
+//
+//  Created by Bacelar on 2018-01-25.
+//  Copyright © 2018 Bacelar. All rights reserved.
+//
+
+import Foundation
+
+
+class Wage {
+    
+    class func getHours(forWage wage:Double, andPrice price: Double) -> Int{
+        return Int(ceil(price / wage))
+    }
+}
